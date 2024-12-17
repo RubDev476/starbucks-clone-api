1ac3d3cc36d9aff4b8ca1ab94e2ad569c2926147
alexisunam <alexisduenasunam@gmail.com>
2024-12-16 23:15:31 -0600

    structuring data in food

--- api/src/routes/menuRoutes.ts
+++ api/src/routes/menuRoutes.ts
@@ -4,6 +4,31 @@ import { getMenu } from '../controllers/menuController';
 const router = Router();
 
 // GET /api/menu
+/**
+ * @openapi
+ * /menu:
+ *  get:
+ *      summary: Obtiene el menú principal.
+ *      responses:
+ *          200:
+ *              description: list of categories
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              type: object
+ *                              properties:
+ *                                  id:
+ *                                      type: integer
+ *                                      example: 1
+ *                                  name:
+ *                                      type: string
+ *                                      example: latte
+ *                                  price:
+ *                                      type: number
+ *                                      example: 40.5
+ */
 router.get('/', getMenu);
 
 export default router;
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,7 @@
 import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
 import menuRoutes from './routes/menuRoutes';
+import { setupSwagger } from './swagger';
 
 dotenv.config();
 
@@ -11,10 +12,14 @@ const PORT = process.env.PORT || 4000;
 // Middlewares
 app.use(express.json());
 
+// Configura Swagger
+setupSwagger(app);
+
 // Rutas
 app.use('/api/menu', menuRoutes);
 
 // Iniciar servidor
 app.listen(PORT, () => {
     console.log(`API running at http://localhost:${PORT}`);
+    console.log(`Swagger Docs en http://localhost:${PORT}/api-docs`);
 });
