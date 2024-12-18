3e337762c7f115ba52dcf54e8a68a2940ff53db1
alexisunam <alexisduenasunam@gmail.com>
2024-12-17 19:04:54 -0600

    Connection succes

---
+++ api/src/settings/db/mongoDb/connection.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+
+    const uri: string = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+    try {
+        await mongoose.connect(uri);
+        console.log('Conectado a MongoDB');
+    } catch (error) {
+        console.error('Error al conectar con MongoDB:', error);
+    }
+};
+
+// Llamar la función de conexión
+connectDB().then(r => r);
+
+export default connectDB;
