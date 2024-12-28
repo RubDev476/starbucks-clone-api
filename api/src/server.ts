0ce650271098eab4665cc2ffbab941f6f02a69d7
alexisunam <alexisduenasunam@gmail.com>
2024-12-27 18:12:54 -0600

    add cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,6 @@
 import express, {Express} from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import menuRoutes from './routes/menuRoutes';
 import { setupSwagger } from './swagger';
 import connectDB from './settings/db/mongoDb/connection';
@@ -10,6 +11,9 @@ dotenv.config();
 
 const app: Express = express();
 const PORT = process.env.PORT || 4000;
+
+// Configurar CORS TODO: Agregar dominio del front-end en producción
+app.use(cors());
 
 // Configurar carpeta estática
 // app.use('/menu-drinks-images', express.static(path.join(__dirname, '../public/fileStorage/menu/drinks')));
